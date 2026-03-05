93b45c3d13acbcaba5b33b6393248786158d33be
Crifer11 <kenneth.chavez@alumnos.udg.mx>
2026-03-04 21:26:00 -0600

    Refactor database connection to use environment variable

--- database.py
+++ database.py
@@ -1,12 +1,5 @@
 import psycopg2
+import os
 
 def conectar_db():
-    return psycopg2.connect(
-        dbname="postgres",
-        user="postgres",
-        password="Equipo123",
-        host="localhost",
-        port="5432"
-    )
-
-conexion = conectar_db()
+    return psycopg2.connect(os.environ["DATABASE_URL"])
