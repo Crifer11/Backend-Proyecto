53746a0c0e51c8c1e60ca6634cf4c8a219ba848c
Crifer11 <kenneth.chavez@alumnos.udg.mx>
2026-03-30 18:23:34 -0600

    Update reportes.py

--- routers/reportes.py
+++ routers/reportes.py
@@ -399,7 +399,7 @@ def descargar_pdf(id_reporte: str = Query(...)):
         
         # Obtener ruta absoluta del logo
         BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-        logo_path = "file://" + os.path.join(BASE_DIR, "logo2.PNG")
+        logo_path = "file://" + os.path.join(BASE_DIR, "logo2.png")
         
         # Convertir rutas relativas a absolutas para WeasyPrint
         if datos["foto_rostro"]:
